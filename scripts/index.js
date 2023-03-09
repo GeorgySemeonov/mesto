3e843060f3f33d0e514f421dda10dbd8b3d06b00
Georgy Semenov <jup1ter97@yandex.ru>
2023-03-09 03:13:43 +0300

    4пр_итерация-2

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 let editProfileButton = document.querySelector(".profile__edit");
 let editPopup = document.querySelector(".popup");
 
-
 editProfileButton.addEventListener("click", function () {
   userNameForm.value = userName.textContent;
   userOccupationForm.value = userOccupation.textContent;
@@ -9,16 +8,14 @@ editProfileButton.addEventListener("click", function () {
   openPopup(editPopup);
 });
 
-function openPopup(popup){
+function openPopup(popup) {
   editPopup.classList.add("popup_opened");
-};
-function closePopup(popup){
+}
+function closePopup(popup) {
   editPopup.classList.remove("popup_opened");
-};
+}
 
-
-
-let closeEditButton = document.querySelector(".popup__close-icon ");
+let closeEditButton = document.querySelector(".popup__close");
 
 closeEditButton.addEventListener("click", function () {
   closePopup(editPopup);
@@ -29,9 +26,7 @@ let userOccupation = document.querySelector(".profile__subtitle");
 
 let userNameForm = document.querySelector("#userNameForm");
 
-
 let userOccupationForm = document.querySelector("#userOccupationForm");
-
 
 let formElement = document.querySelector("#popupForms");
 

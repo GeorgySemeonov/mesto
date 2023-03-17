8eb4d22efa3e35efbfdaf85e6a06d3e862f0f36c
Georgy Semenov <jup1ter97@yandex.ru>
2023-03-18 01:21:09 +0300

    5пр итерация

--- scripts/index.js
+++ scripts/index.js
@@ -14,9 +14,6 @@ editProfileButton.addEventListener("click", () => {
 });
 
 createCardButton.addEventListener("click", () => {
-  // userNameForm.value = userName.textContent;
-  // userOccupationForm.value = userOccupation.textContent;
-
   openPopup(createCardPopup);
 });
 
@@ -83,65 +80,58 @@ const initialCards = [
 const cardsList = document.querySelector(".elements__list");
 
 const createCard = (card) => {
-
-  const cardTamplate = document.querySelector("#cardTamplate").content.cloneNode(true);
+  const cardTamplate = document
+    .querySelector("#cardTamplate")
+    .content.cloneNode(true);
   const cardHeading = cardTamplate.querySelector(".element__title");
   cardHeading.textContent = card.name;
   const cardImage = cardTamplate.querySelector(".element__image");
-  cardImage.setAttribute('src', card.link )
-  const deliteButton = cardTamplate.querySelector('.element__delite-button');
-  deliteButton.addEventListener('click', deliteCard );
-  const likeButton = cardTamplate.querySelector('.element__like-button');
-  likeButton.addEventListener('click', likeCard) ;
-  // const popupImage = document.querySelector('.element__image');
-  cardImage.addEventListener('click', imageSubmit);
+  cardImage.setAttribute("src", card.link);
+  const deliteButton = cardTamplate.querySelector(".element__delite-button");
+  deliteButton.addEventListener("click", deliteCard);
+  const likeButton = cardTamplate.querySelector(".element__like-button");
+  likeButton.addEventListener("click", likeCard);
+  cardImage.addEventListener("click", imageSubmit);
 
   cardsList.prepend(cardTamplate);
-  
-}
+};
 
 initialCards.forEach(createCard);
-// initialCards.push(createCard);
 
-function deliteCard (event){
-  const button = event.target
-  const card = button.closest('.element')
-  card.remove()
+function deliteCard(event) {
+  const button = event.target;
+  const card = button.closest(".element");
+  card.remove();
 }
 
-const popupFormsCard = document.querySelector('#popupFormsCard');
-popupFormsCard.addEventListener('submit', cardFormSubmit)
+const popupFormsCard = document.querySelector("#popupFormsCard");
+popupFormsCard.addEventListener("submit", cardFormSubmit);
 
 function cardFormSubmit(event) {
-  event.preventDefault()
-  const popupFormsCard = event.target
-  const nameCard = document.querySelector('#nameCard').value
-  const imageLink = document.querySelector('#imageLink').value
-  const card = { 
+  event.preventDefault();
+  const popupFormsCard = event.target;
+  const nameCard = document.querySelector("#nameCard").value;
+  const imageLink = document.querySelector("#imageLink").value;
+  const card = {
     name: nameCard,
-    link: imageLink
-  }
-  createCard(card)
+    link: imageLink,
+  };
+  createCard(card);
   closePopup(cardPopup);
 }
 
 initialCards.push(cardFormSubmit);
 
-function likeCard (event){
-  event.target.classList.toggle('element__like-icon_active')
+function likeCard(event) {
+  event.target.classList.toggle("element__like-icon_active");
 }
 
-// evt.target.classList.toggle('song__like_active');
-
-// function likeCard(evt){evt.target.closest('.container__button').classList.toggle('container__button_active');};
-
-
-
-
 function imageSubmit(event) {
-const image = document.querySelector('.popup__image');
-image.src = event.target.src;
-const popupTitle = document.querySelector('.popup__image-title');
-popupTitle.textContent = event.target.closest('.element').querySelector('.element__title').textContent;
-openPopup(openImagePopup);
+  const image = document.querySelector(".popup__image");
+  image.src = event.target.src;
+  const popupTitle = document.querySelector(".popup__image-title");
+  popupTitle.textContent = event.target
+    .closest(".element")
+    .querySelector(".element__title").textContent;
+  openPopup(openImagePopup);
 }

c039a75a1a5dcaf8e20dd115d8efc9f2825a636c
Georgy Semenov <jup1ter97@yandex.ru>
2023-03-19 02:02:47 +0300

    5пр итерация_1

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,28 @@
 const popup = document.querySelector(".popup");
 
+
+
 const editProfileButton = document.querySelector(".profile__edit");
 const profilePopup = document.querySelector("#profilePopup");
 const createCardButton = document.querySelector(".profile__add-button");
 const createCardPopup = document.querySelector("#cardPopup");
 const openImagePopup = document.querySelector("#imagePopup");
+
+const closeButtons = document.querySelectorAll(".popup__close");
+const userName = document.querySelector(".profile__title");
+const userOccupation = document.querySelector(".profile__subtitle");
+const userNameForm = document.querySelector("#userNameForm");
+const userOccupationForm = document.querySelector("#userOccupationForm");
+const formElement = document.querySelector("#popupForms");
+
+const cardsList = document.querySelector(".elements__list");
+const popupFormsCard = document.querySelector("#popupFormsCard");
+
+const image = document.querySelector(".popup__image");
+const popupTitle = document.querySelector(".popup__image-title");
+
+const nameCard = document.querySelector("#nameCard");
+const imageLink = document.querySelector("#imageLink");
 
 editProfileButton.addEventListener("click", () => {
   userNameForm.value = userName.textContent;
@@ -24,21 +42,12 @@ function closePopup(popup) {
   popup.classList.remove("popup_opened");
 }
 
-const closeButtons = document.querySelectorAll(".popup__close");
+
 
 closeButtons.forEach((button) => {
   const popup = button.closest(".popup");
   button.addEventListener("click", () => closePopup(popup));
 });
-
-let userName = document.querySelector(".profile__title");
-let userOccupation = document.querySelector(".profile__subtitle");
-
-let userNameForm = document.querySelector("#userNameForm");
-
-let userOccupationForm = document.querySelector("#userOccupationForm");
-
-let formElement = document.querySelector("#popupForms");
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
@@ -77,26 +86,27 @@ const initialCards = [
   },
 ];
 
-const cardsList = document.querySelector(".elements__list");
-
-const createCard = (card) => {
-  const cardTamplate = document
-    .querySelector("#cardTamplate")
-    .content.cloneNode(true);
+const newCard = (card) => {
+  const cardTamplate = document.querySelector("#cardTamplate").content.cloneNode(true); 
   const cardHeading = cardTamplate.querySelector(".element__title");
   cardHeading.textContent = card.name;
   const cardImage = cardTamplate.querySelector(".element__image");
   cardImage.setAttribute("src", card.link);
+  cardImage.setAttribute("alt", card.name);
   const deliteButton = cardTamplate.querySelector(".element__delite-button");
   deliteButton.addEventListener("click", deliteCard);
   const likeButton = cardTamplate.querySelector(".element__like-button");
   likeButton.addEventListener("click", likeCard);
   cardImage.addEventListener("click", imageSubmit);
-
-  cardsList.prepend(cardTamplate);
+  
+  
+  cardsList.prepend(cardTamplate); 
 };
 
-initialCards.forEach(createCard);
+
+initialCards.forEach(newCard);
+
+
 
 function deliteCard(event) {
   const button = event.target;
@@ -104,19 +114,16 @@ function deliteCard(event) {
   card.remove();
 }
 
-const popupFormsCard = document.querySelector("#popupFormsCard");
 popupFormsCard.addEventListener("submit", cardFormSubmit);
 
 function cardFormSubmit(event) {
   event.preventDefault();
   const popupFormsCard = event.target;
-  const nameCard = document.querySelector("#nameCard").value;
-  const imageLink = document.querySelector("#imageLink").value;
   const card = {
-    name: nameCard,
-    link: imageLink,
+    name: nameCard.value,
+    link: imageLink.value,
   };
-  createCard(card);
+  newCard(card);
   closePopup(cardPopup);
 }
 
@@ -126,10 +133,11 @@ function likeCard(event) {
   event.target.classList.toggle("element__like-icon_active");
 }
 
+
 function imageSubmit(event) {
-  const image = document.querySelector(".popup__image");
+  
   image.src = event.target.src;
-  const popupTitle = document.querySelector(".popup__image-title");
+  image.setAttribute("alt", popupTitle.textContent);
   popupTitle.textContent = event.target
     .closest(".element")
     .querySelector(".element__title").textContent;
